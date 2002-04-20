8614864becb254972ff99813d9575d5f17608c18
Curt Mills <curt.we7u@gmail.com>
2002-04-20 19:23:54 +0000

    Fix for changing line widths on shapefile maps.

--- src/maps.c
+++ src/maps.c
@@ -1646,8 +1646,10 @@ void draw_shapefile_map (Widget w,
                             (void)XSetLineAttributes (XtDisplay (w), gc, lanes, LineSolid, CapButt,JoinMiter);
                         }
                     }
-
-                    if (water_flag) {
+                    else if (water_flag) {
+                        (void)XSetLineAttributes (XtDisplay (w), gc, 0, LineSolid, CapButt,JoinMiter);
+                    }
+                    else {  // Set default line width
                         (void)XSetLineAttributes (XtDisplay (w), gc, 0, LineSolid, CapButt,JoinMiter);
                     }
 
@@ -1742,6 +1744,7 @@ void draw_shapefile_map (Widget w,
                             } else {
                                 int temp;
 
+                                (void)XSetLineAttributes (XtDisplay (w), gc, 0, LineSolid, CapButt,JoinMiter);
                                 temp = XDrawLines(XtDisplay(w), pixmap, gc, points, i, CoordModeOrigin);
                                 if (temp != 0) {
                                     //printf("*** BAD XDrawLines return value: %d ***\n", temp);
