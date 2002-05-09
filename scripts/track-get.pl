288914e141b7b3c8f88fb8c74b47ead2295a77c0
cvs2git <admin@example.com>
2002-05-09 20:28:13 +0000

    This commit was manufactured by cvs2git to create tag 'we7u-safe'.
    Sprout from master 2002-04-12 22:49:01 UTC Curt Mills <curt.we7u@gmail.com> 'Changing to capital 'X' for the weather-station-type designator in our'
    Cherrypick from master 2002-05-09 20:28:12 UTC Curt Mills <curt.we7u@gmail.com> 'Tweaks to make the appropriate scripts get installed in /usr/local/bin,':
        scripts/Makefile.am
        scripts/Makefile.in
        scripts/track-get.pl
        scripts/waypoint-get.pl
        symbols/2x2.xbm
        symbols/alert.xbm
        symbols/flood.xbm
        symbols/snow.xbm
        symbols/tornado.xbm
        symbols/wind.xbm
        symbols/winter_wx.xbm
        symbols/wntr_strm.xbm

---
+++ scripts/track-get.pl
@@ -0,0 +1,145 @@
+#!/usr/bin/perl
+
+# $Id$
+
+# This script will ask for an item designator, then create a file
+# with that name + ".log" in the ~/.xastir/logs directory.  The
+# file will contain APRS items created from the downloaded Garmin
+# tracklog.  Reading that log file with Xastir will result in an
+# object with a track being displayed on the map screen.
+
+# You may wish to bump up MAX_TRACKS in db.h to 1024 in order to see
+# the entire log, but be careful with Xastir's memory image growing
+# too fast if you're connected to the 'net after making this change.
+
+# This script uses the GPS::Garmin module which can be obtained
+# from http://sourceforge.net/projects/perl-gps/
+
+
+use GPS::Garmin;
+use IO::File;
+
+
+# Flush STDOUT
+$| = 1;
+
+
+# Hard-coded "from" callsign name.  This becomes the "sending"
+# callsign for the APRS Items.
+$from = "SAR";
+
+
+# Ask for the item name.  This will turn into the 3-9 character APRS
+# item name.
+printf("\n\nEnter item designator: Length 1-9, all except '!' and '_' allowed, case respected:\n\n");
+printf("\t123456789\n\t");
+$name = <>;
+chomp $name;
+if (length($name) < 1) {
+  die "Must be at least one character!\n";
+}
+if (length($name) > 9) {
+  $name =~ s/(.{9}).*/\1/;  # Terminate it at 9 chars
+}
+if (length($name) < 3) {
+  $name = $name . "   ";    # Pad the name with spaces
+  $name =~ s/(.{3}).*/\1/;  # Terminate it at 3 chars
+}
+
+# We need to enforce the naming restrictions for APRS Items.  '!'
+# and '_' are not allowed, anything else that's printable ASCII is
+# ok.
+@j = split(//, $name);
+for ($i = 0; $i < length($name); $i++) {
+#printf("$i  $j[$i]\n");
+  if (     ($j[$i] lt ' ')
+        || ($j[$i] gt '~')
+        || ($j[$i] eq '!')
+        || ($j[$i] eq '_') ) {
+    $j[$i] = ' ';
+  }
+}
+$name = join("",@j);
+
+$filename = "~/.xastir/logs/$name.log";
+
+# Get rid of spaces in the filename
+$filename =~ s/\s+//g;
+
+# Expand the tilde into the home directory
+$filename =~ s{ ^ ~ ( [^/]* ) }
+              { $1
+                    ? (getpwnam($1))[7]
+                    : ( $ENV{HOME} || $ENV{LOGDIR}
+                         || (getpwuid($>))[7]
+                      )
+}ex;
+
+printf("\nThe item designator will be: ($name)\n");
+printf("The output filename will be: ($filename)\n\n");
+printf("Connect Garmin GPS to /dev/ttyS0 (COM1), set it to Garmin/Garmin mode.\n");
+printf("Press <ENTER> to proceed with download, Ctrl-C to abort\n");
+<>;
+
+
+# Create a file to hold the data
+$output = IO::File->new("> $filename")
+  or die "Couldn't open $filename for writing: $!\n";
+
+
+$gps = new GPS::Garmin( 'Port' => '/dev/ttyS0',
+                       'Baud' => 9600,
+                      ) or die "Unable to connect to receiver: $!";
+
+
+# Transfer trackpoints:
+$i = 0;
+$gps->prepare_transfer("trk");
+while ($gps->records)
+{
+  ($lat,$lon,$time) = $gps->grab;
+#  printf("%f %f %d\n",$lat,$lon,$time);
+  if ($lon < 0) {
+    $londeg = sprintf("%d",-$lon);
+    $lonmin = (-$lon - $londeg) * 60;
+    $lonchar = "W";
+  }
+  else {
+    $londeg = sprintf("%d",$lon);
+    $lonchar = "E";
+    $lonmin = ($lon - $londeg) * 60;
+  }
+  if ($lat < 0) {
+    $latdeg = sprintf("%d",-$lat);
+    $latmin = (-$lat - $latdeg) * 60;
+    $latchar = "S";
+  }
+  else {
+    $latdeg = sprintf("%d",$lat);
+    $latmin = ($lat - $latdeg) * 60;
+    $latchar = "N";
+  }
+
+  # Write an APRS Item for each trackpoint
+  printf($output "%s>APRS:)%s!%02d%05.2f%s/%03d%05.2f%s%s\n",
+    $from,
+    $name,
+    $latdeg,
+    $latmin,
+    $latchar,
+    $londeg,
+    $lonmin,
+    $lonchar,
+    "/");
+
+  if (! ($i % 10) ) {
+    printf("$i ");
+  }
+  $i++;
+}
+
+printf("\n\nThe data has been saved in: ($filename)\n");
+printf("Please open this logfile with Xastir to display the track\n");
+printf("!!!Remember to set your GPS back to NMEA mode for APRS!!!\n");
+
+
---
+++ scripts/waypoint-get.pl
@@ -0,0 +1,147 @@
+#!/usr/bin/perl
+
+# $Id$
+
+# This script will ask for a name, then create a file
+# with that name + ".log" in the ~/.xastir/logs directory.  The
+# file will contain APRS items created from the downloaded Garmin
+# waypoints.  Reading that log file with Xastir will result in one
+# item appearing on the map screen for each waypoint, labeled with
+# the name of that waypoint.
+
+# This script uses the GPS::Garmin module which can be obtained
+# from http://sourceforge.net/projects/perl-gps/
+
+
+use GPS::Garmin;
+use IO::File;
+
+
+# Flush STDOUT
+$| = 1;
+
+
+# Hard-coded "from" callsign name.  This becomes the "sending"
+# callsign for the APRS Items.
+$from = "SAR";
+
+
+# Ask for the name.  This gets changed into the complete log file name.
+printf("\n\nEnter name for file: '.log' will get added to the end:\n\n");
+$name = <>;
+chomp $name;
+if (length($name) < 1) {
+  die "Must be at least one character!\n";
+}
+
+$filename = "~/.xastir/logs/$name.log";
+
+# Get rid of spaces in the filename
+$filename =~ s/\s+//g;
+
+# Expand the tilde into the home directory
+$filename =~ s{ ^ ~ ( [^/]* ) }
+              { $1
+                    ? (getpwnam($1))[7]
+                    : ( $ENV{HOME} || $ENV{LOGDIR}
+                         || (getpwuid($>))[7]
+                      )
+}ex;
+
+printf("\nThe output filename will be: ($filename)\n\n");
+printf("Connect Garmin GPS to /dev/ttyS0 (COM1), set it to Garmin/Garmin mode.\n");
+printf("Press <ENTER> to proceed with download, Ctrl-C to abort\n");
+<>;
+
+
+# Create a file to hold the data
+$output = IO::File->new("> $filename")
+  or die "Couldn't open $filename for writing: $!\n";
+
+
+$gps = new GPS::Garmin( 'Port' => '/dev/ttyS0',
+                       'Baud' => 9600,
+                      ) or die "Unable to connect to receiver: $!";
+
+
+# Transfer waypoints:
+$i = 0;
+$gps->prepare_transfer("wpt");
+while ($gps->records)
+{
+  ($title,$lat,$lon,$desc) = $gps->grab;
+
+  #printf("%f\t%f\t%s\t%s\n",$lat,$lon,$desc,$title);
+
+  if ($lon < 0) {
+    $londeg = sprintf("%d",-$lon);
+    $lonmin = (-$lon - $londeg) * 60;
+    $lonchar = "W";
+  }
+  else {
+    $londeg = sprintf("%d",$lon);
+    $lonchar = "E";
+    $lonmin = ($lon - $londeg) * 60;
+  }
+  if ($lat < 0) {
+    $latdeg = sprintf("%d",-$lat);
+    $latmin = (-$lat - $latdeg) * 60;
+    $latchar = "S";
+  }
+  else {
+    $latdeg = sprintf("%d",$lat);
+    $latmin = ($lat - $latdeg) * 60;
+    $latchar = "N";
+  }
+
+  chomp $title;
+  if (length($title) < 1) {
+    die "Must be at least one character!\n";
+  }
+  if (length($title) > 9) {
+    $title =~ s/(.{9}).*/\1/;  # Terminate it at 9 chars
+  }
+  if (length($title) < 3) {
+    $title = $title . "   ";    # Pad the title with spaces
+    $title =~ s/(.{3}).*/\1/;  # Terminate it at 3 chars
+  }
+
+  # We need to enforce the naming restrictions for APRS Items.  '!'
+  # and '_' are not allowed, anything else that's printable ASCII is
+  # ok.
+  @j = split(//, $title);
+  for ($k = 0; $k < length($title); $k++) {
+  #printf("$k  $j[$k]\n");
+    if (     ($j[$k] lt ' ')
+          || ($j[$k] gt '~')
+          || ($j[$k] eq '!')
+          || ($j[$k] eq '_') ) {
+      $j[$k] = ' ';
+    }
+  }
+  $title = join("",@j);
+
+  # Write an APRS Item for each waypoint
+  printf($output "%s>APRS:)%s!%02d%05.2f%s/%03d%05.2f%s%s\n",
+    $from,
+    $title,
+    $latdeg,
+    $latmin,
+    $latchar,
+    $londeg,
+    $lonmin,
+    $lonchar,
+    "/");
+
+  if (! ($i % 10) ) {
+    printf("$i ");
+  }
+
+  $i++;
+}
+
+printf("\n\nThe data has been saved in: ($filename)\n");
+printf("Please open this logfile with Xastir to display the waypoints.\n");
+printf("!!!Remember to set your GPS back to NMEA mode for APRS!!!\n");
+
+
