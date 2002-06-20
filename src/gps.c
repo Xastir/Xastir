e130a1a4a08959fc3cecb7ab4a95799d97b9aada
Curt Mills <curt.we7u@gmail.com>
2002-06-20 17:44:23 +0000

    Added some notes to the waypoint creation routine.

--- src/gps.c
+++ src/gps.c
@@ -485,6 +485,23 @@ char *nmea_checksum(char *nmea_sentence) {
 // WPTNME       Waypoint Name (stick to 6 chars for compatibility?)
 // *31          Checksum, always begins with '*'
 //
+//
+// Future implementation ideas:
+//
+// Create linked list of waypoints/location.
+// Use the list to prevent multiple writes of the same waypoint if
+// nothing has changed.
+//
+// Use the list to check distance of previously-written waypoints.
+// If we're out of range, delete the waypoint and remove it from the
+// list.
+//
+// Perhaps write the list to disk also.  As we shut down, delete the
+// waypoints (self-cleaning).  As we come up, load them in again?
+// We could also just continue cleaning out waypoints that are
+// out-of-range since the last time we ran the program.  That's
+// probably a better solution.
+//
 void create_garmin_waypoint(long latitude,long longitude,char *call_sign) {
     char short_callsign[10];
     char lat_string[15];
