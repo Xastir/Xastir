8feb24dcb94229dbfe3a1a959139dc1ded3f8dc5
Curt Mills <curt.we7u@gmail.com>
2002-09-19 08:08:50 +0000

    Adding new language string for TrackMe button.

--- src/main.c
+++ src/main.c
@@ -4970,8 +4970,7 @@ void create_appshell( /*@unused@*/ Display *display, char *app_name, /*@unused@*
             MY_BACKGROUND_COLOR,
             NULL);
 
-//    trackme_button=XtVaCreateManagedWidget(langcode("TrackMe"),
-    trackme_button=XtVaCreateManagedWidget("TrackMe",
+    trackme_button=XtVaCreateManagedWidget(langcode("POPUPMA022"),
             xmToggleButtonGadgetClass,
             trackme_frame,
             XmNtopAttachment, XmATTACH_FORM,
