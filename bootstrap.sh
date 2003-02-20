61dca4a6a7c197a09cf4d598fac705efff68694d
Curt Mills <curt.we7u@gmail.com>
2003-02-20 10:10:45 +0000

    Added status messages.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,16 @@
 # This simple routine will run autostuff in the appropriate
 # order to generate the needed configure/makefiles
 #
-
+echo "Running aclocal"
 aclocal
 
+echo "Running autoheader"
 autoheader
 
+echo "Running autoconf"
 autoconf
 
 # Cygwin needs these params to be separate
+echo "Running automake"
 automake -a -c
 
