ae6df535dfb2815ba32f446d114a51c4423135a7
Curt Mills <curt.we7u@gmail.com>
2003-02-20 06:47:11 +0000

    Separating out the -a and -c params on the automake line, for Cygwin.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,13 @@
 # This simple routine will run autostuff in the appropriate
 # order to generate the needed configure/makefiles
 #
+
 aclocal
+
 autoheader
+
 autoconf
-automake -ac
+
+# Cygwin needs these params to be separate
+automake -a -c
 
