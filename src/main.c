da96082a33f5113092c4f7357e442abefe1e609e
Curt Mills <curt.we7u@gmail.com>
2003-04-02 19:30:50 +0000

    Disabling my test_create_shapefile_map entry.  Some people can't get Xastir to compile with it in there, and it's test code anyway.

--- src/main.c
+++ src/main.c
@@ -13544,7 +13544,7 @@ void Test(Widget w, XtPointer clientData, XtPointer callData) {
 
     (void)XCopyArea(XtDisplay(da),pixmap_final,XtWindow(da),gc,0,0,screen_width,screen_height,0,0);
 
-    test_create_shapefile_map();
+    //test_create_shapefile_map();
 }
 
 
