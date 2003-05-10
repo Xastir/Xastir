5c79cac4d2a6f4e6f56dd01aae17ea259b0bb63a
Curt Mills <curt.we7u@gmail.com>
2003-05-10 07:31:07 +0000

    Converting one more strcpy to strncpy.

--- src/alert.c
+++ src/alert.c
@@ -319,7 +319,7 @@ void normal_title(char *incoming_title, char *outgoing_title) {
     outgoing_title[32] = '\0';
     if ((c_ptr = strstr(outgoing_title, "County Warning Area ")) && c_ptr == outgoing_title) {
         c_ptr = &outgoing_title[strlen("County Warning Area ")]; // Find end of text
-        strcpy(outgoing_title, "CWA");  // Add "CWA" to output string instead
+        strncpy(outgoing_title, "CWA",4);  // Add "CWA" to output string instead
         // Copy remaining portion of input string to the output string
         strncat(outgoing_title, c_ptr, 32-strlen("County Warning Area "));
         outgoing_title[32] = '\0';  // Make sure string is terminated
