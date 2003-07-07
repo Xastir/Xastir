b393c4f81df25d3130e7667e2fdb44b342da2f0b
Curt Mills <curt.we7u@gmail.com>
2003-07-07 18:09:34 +0000

    Script now removes the map_index.sys file and moves the selected_maps.sys file to selected_maps.sys.backup

--- scripts/xastir-fixcfg.sh
+++ scripts/xastir-fixcfg.sh
@@ -5,9 +5,13 @@
 # fix up a user's .xastir/config/xastir.cnf to rewrite /usr/local/xastir
 # to /usr/local/share/xastir.  NOTE: This only works when -prefix=/usr/local!
 CNF=.xastir/config/xastir.cnf
+INDEX=.xastir/config/map_index.sys
+SELECT=.xastir/config/selected_maps.sys
 cd
 if [ -f $CNF ]; then
+    rm $INDEX
     mv $CNF $CNF.backup
+    mv $SELECT $SELECT.backup
     if [ $? -ne 0 ]; then
 	echo "$CNF: unable to rename!"
 	exit 1
