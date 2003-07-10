cb69982685c1cfbcab5dacbb836adf868b1504f3
Curt Mills <curt.we7u@gmail.com>
2003-07-10 20:25:47 +0000

    Timezone changes for Cygwin.

--- src/util.c
+++ src/util.c
@@ -756,8 +756,16 @@ time_t time_from_aprsstring(char *aprs_time) {
 #ifndef __CYGWIN__
     extern time_t timezone;
 #endif  // __CYGWIN__
+    // Use "_timezone" instead in Cygwin
+#define timezone _timezone
 #endif  // HAVE_TM_GMTOFF
 
+
+#ifdef __CYGWIN__
+    // Must call tzset before using the _timezone variable in
+    // Cygwin, else the timezone won't have been initialized.
+    tzset();
+#endif  // __CYGWIN__
 
     // Compute our current time and the offset from GMT.  If
     // daylight savings time is in effect, factor that in as well.
