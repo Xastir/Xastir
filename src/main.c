862b4a6e8da3b4e9cef7b9843e256f8c6c9ea794
Curt Mills <curt.we7u@gmail.com>
2003-09-04 17:11:12 +0000

    Adding an option for a much smaller default system font.  Useful for smaller displays.

--- src/main.c
+++ src/main.c
@@ -140,7 +140,16 @@
 
 // If next line uncommented, Xastir will use larger fonts nearly
 // everywhere, including menus and status bar.
+//
 //#define USE_LARGE_SYSTEM_FONT
+
+// Conversely, this will make the system font a bit smaller than
+// normal.  Only define one or zero of USE_LARGE_SYSTEM_FONT or
+// USE_SMALL_SYSTEM_FONT.
+//
+//#define USE_SMALL_SYSTEM_FONT
+
+
 
 // If next line uncommented, Xastir will use a large font for the
 // station text in the drawing area.
@@ -22486,6 +22495,10 @@ int main(int argc, char *argv[]) {
 #ifdef USE_LARGE_SYSTEM_FONT
         "*.fontList: -*-*-*-*-*-*-20-*-*-*-*-*-*-*\n",
 #endif  // USE_LARGE_SYSTEM_FONT
+
+#ifdef USE_SMALL_SYSTEM_FONT
+        "*.fontList: -*-*-*-*-*-*-8-*-*-*-*-*-*-*\n",
+#endif  // USE_SMALL_SYSTEM_FONT
 
         "*List.Translations: #override \n\
         <Key>Return:    Select(children)\n\
