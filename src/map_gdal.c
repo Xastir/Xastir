ff094d8e1d64ed56c7a719a1e5b2ecb49bb1a938
Curt Mills <curt.we7u@gmail.com>
2003-10-16 16:42:46 +0000

    Added example code snippets from the GDAL tutorial.

--- src/map_gdal.c
+++ src/map_gdal.c
@@ -125,15 +125,106 @@ void map_gdal_init() {
 #ifdef HAVE_LIBGDAL
 
 int gdal_main() {
-//    GDALDatasetH  hDataset;
+/*
+    GDALDatasetH  hDataset;
 
-//    hDataset = GDALOpen( pszFilename, GA_ReadOnly );
-//    if( hDataset == NULL )
+    hDataset = GDALOpen( pszFilename, GA_ReadOnly );
+    if( hDataset == NULL )
     {
-//        ...;
+        ...;
     }
+
+    GDALClose(hDataset);
+*/
+
     return(0);
 }
+
+
+
+
+
+/*
+// Getting Dataset Information, example code:
+
+    GDALDriverH   hDriver;
+    double adfGeoTransform[6];
+
+    hDriver = GDALGetDatasetDriver( hDataset );
+    printf( "Driver: %s/%s\n",
+        GDALGetDriverShortName( hDriver ),
+        GDALGetDriverLongName( hDriver ) );
+
+    printf( "Size is %dx%dx%d\n",
+        GDALGetRasterXSize( hDataset ), 
+        GDALGetRasterYSize( hDataset ),
+        GDALGetRasterCount( hDataset ) );
+
+    if( GDALGetProjectionRef( hDataset ) != NULL )
+        printf( "Projection is `%s'\n", GDALGetProjectionRef( hDataset ) );
+
+    if( GDALGetGeoTransform( hDataset, adfGeoTransform ) == CE_None) {
+        printf( "Origin = (%.6f,%.6f)\n",
+            adfGeoTransform[0], adfGeoTransform[3] );
+
+        printf( "Pixel Size = (%.6f,%.6f)\n",
+            adfGeoTransform[1], adfGeoTransform[5] );
+    }
+*/
+
+
+
+
+
+/*
+// Fetching a raster band, example code:
+
+    GDALRasterBandH hBand;
+    int nBlockXSize, nBlockYSize;
+    int bGotMin, bGotMax;
+    double adfMinMax[2];
+        
+    hBand = GDALGetRasterBand( hDataset, 1 );
+    GDALGetBlockSize( hBand, &nBlockXSize, &nBlockYSize );
+    printf( "Block=%dx%d Type=%s, ColorInterp=%s\n",
+        nBlockXSize, nBlockYSize,
+        GDALGetDataTypeName(GDALGetRasterDataType(hBand)),
+        GDALGetColorInterpretationName(
+            GDALGetRasterColorInterpretation(hBand)) );
+
+    adfMinMax[0] = GDALGetRasterMinimum( hBand, &bGotMin );
+    adfMinMax[1] = GDALGetRasterMaximum( hBand, &bGotMax );
+    if( ! (bGotMin && bGotMax) )
+        GDALComputeRasterMinMax( hBand, TRUE, adfMinMax );
+
+    printf("Min=%.3fd, Max=%.3f\n", adfMinMax[0], adfMinMax[1]);
+        
+    if( GDALGetOverviewCount(hBand) > 0 )
+        printf( "Band has %d overviews.\n", GDALGetOverviewCount(hBand));
+
+    if( GDALGetRasterColorTable( hBand ) != NULL )
+        printf( "Band has a color table with %d entries.\n", 
+            GDALGetColorEntryCount(
+                GDALGetRasterColorTable( hBand ) ) );
+*/
+
+
+
+
+
+/* Reading Raster Data, example code:
+
+    float *pafScanline;
+    int nXSize = GDALGetRasterBandXSize( hBand );
+
+    pafScanline = (float *) CPLMalloc(sizeof(float)*nXSize);
+    GDALRasterIO( hBand, GF_Read, 0, 0, nXSize, 1, 
+        pafScanline, nXSize, 1, GDT_Float32, 
+        0, 0 );
+*/
+
+
+
 
 
 #endif // HAVE_LIBGDAL
