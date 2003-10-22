bb7f0f5df4000621e6cb1759a272f56a9e55820b
Curt Mills <curt.we7u@gmail.com>
2003-10-22 23:19:26 +0000

    Commenting out some debug lines.

--- src/maps.c
+++ src/maps.c
@@ -1174,7 +1174,11 @@ void draw_grid(Widget w) {
                         nbp++;
                     else if (nbp > 0) { // We had a boundary point, but not anymore
                         fp = utm_grid.zone[zone].col[i].firstpoint = j - 1;
+//fprintf(stderr,"np:%d, j:%d\n",np,j);
                         np = utm_grid.zone[zone].col[i].npoints    = np - j + 1;
+//fprintf(stderr,"new np:%d\n",np);
+                        if (np < 0)
+                            np = 0;
                         break;
                     }
                     if (nbp == np) { // All points are boundary points
@@ -1197,8 +1201,8 @@ void draw_grid(Widget w) {
                         fp = utm_grid.zone[zone].col[i].firstpoint = 0;
                     }
                     else {
-fprintf(stderr,"draw_grid: i:%d, np:%d, size:%d\n",i,np,sizeof(XPoint));
-fprintf(stderr,"Problem: in draw_grid() memmove, np was %d.  Skipping memmove.\n",np);
+//fprintf(stderr,"draw_grid: i:%d, np:%d, size:%d\n",i,np,sizeof(XPoint));
+//fprintf(stderr,"Problem: in draw_grid() memmove, np was %d.  Skipping memmove.\n",np);
                     }
                 }
 
