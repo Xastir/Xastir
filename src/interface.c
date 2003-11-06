d8f0d6f2dce91707ed3da4fce8f1bb45bf10c367
Curt Mills <curt.we7u@gmail.com>
2003-11-06 17:21:40 +0000

    OpenTrac bug-fix by Henk.  Thanks!

--- src/interface.c
+++ src/interface.c
@@ -1259,7 +1259,7 @@ int OpenTrac_decode_origination(unsigned char *element,
 
     *sequence = fetch16bits(element+6);
     if (element_len == 9) {
-        *network = *(element+9);
+        *network = *(element+8);
 
         fprintf(stderr, "Origin: %s-%d seq %d net %d\n",
             callsign,
