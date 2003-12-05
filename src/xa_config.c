c84b71a577bd4eee56f68878f3f18c95b76b5db7
Curt Mills <curt.we7u@gmail.com>
2003-12-05 22:07:05 +0000

    Changing the default serial_char_pacing to 1ms instead of 25ms.

--- src/xa_config.c
+++ src/xa_config.c
@@ -1440,8 +1440,8 @@ void load_data_or_default(void) {
     if (!get_int ("DEAD_RECKONING_TIMEOUT", &dead_reckoning_timeout,1,60*60*24*30,60*10))
         dead_reckoning_timeout = 60*10;     // Ten minute default
 
-    if (!get_int ("SERIAL_CHAR_PACING", &serial_char_pacing,0,50,25))
-        serial_char_pacing = 25;    // 25ms default
+    if (!get_int ("SERIAL_CHAR_PACING", &serial_char_pacing,0,50,1))
+        serial_char_pacing = 1; // 1ms default
 
 
 
