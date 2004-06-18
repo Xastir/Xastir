23030091ef613ed1150c272570d7052a61c609e5
Curt Mills <curt.we7u@gmail.com>
2004-06-18 15:24:33 +0000

    Adding in catch-all clauses for OpenTrac elements that are new or not-yet-implemented.

--- src/interface.c
+++ src/interface.c
@@ -2827,7 +2827,22 @@ fprintf(stderr, "\n***** %s\n\n", buffer);
                         "Unknown Element Type, ");
 
                     // Provide more data if known.
-                    if ((etype & 0xff00) == 0x100) {
+                    if ((etype & 0xfff0) == 0x000) {
+                        fprintf(stderr, "Classification: Stream Control\n");
+                    }
+                    else if ((etype & 0xfff0) == 0x10) {
+                        fprintf(stderr, "Classification: Common Attribute\n");
+                    }
+                    else if ((etype & 0xfff0) == 0x20) {
+                        fprintf(stderr, "Classification: Network Information\n");
+                    }
+                    else if ((etype & 0xfff0) == 0x30) {
+                        fprintf(stderr, "Classification: ??\n");
+                    }
+                    else if ((etype & 0xfff0) == 0x40) {
+                        fprintf(stderr, "Classification: Weather\n");
+                    }
+                    else if ((etype & 0xff00) == 0x100) {
                         fprintf(stderr, "Classification: Reserved for Status Flags\n");
                     }
                     else if ((etype & 0xff00) == 0x200) {
