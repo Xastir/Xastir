108ed813dcb1459970c032d3f7b9ec5cb019fc8a
Curt Mills <curt.we7u@gmail.com>
2004-07-20 18:14:27 +0000

    Bumping max status/comment lines per station from 10 to 20.  Am testing with 50 locally to see whether bumping it up more will seriously affect memory usage.  Depending on the results, may bump these numbers up even more.

--- src/db.h
+++ src/db.h
@@ -33,9 +33,9 @@
 
 #define MSG_INCREMENT 200
 #define MAX_CALLSIGN 9       // Objects are up to 9 chars
-#define MAX_TACTICAL_CALL 20 // Up to 20 chars for tactical calls
-#define MAX_COMMENT_LINES 10  // Save up to 10 unique comment strings per station
-#define MAX_STATUS_LINES 10   // Save up to 10 unique status strings per station
+#define MAX_TACTICAL_CALL 20 // Up to XX chars for tactical calls
+#define MAX_COMMENT_LINES 20  // Save XX unique comment strings per station
+#define MAX_STATUS_LINES 20   // Save XX unique status strings per station
 
 /* define max tnc line size (should be from tnc.h) */
 #define MAX_TNC_LINE_SIZE 300
