01c6a61426c9ef1d7e0432a0b74c700fa8690486
Tom Russo <tom.km5vy@gmail.com>
2004-09-17 17:12:28 +0000

    Put Cygwin-specific setting of nexttime into the UpdateTime function.
    Remove the two Cygwin-specific usleep(2) from the end of UpdateTime.
    
    With these changes, running xastir on Cygwin under Win2K on a 2GHz machine
    went from taking up 90+% of the CPU to taking around 20% --- most of that
    in system processes, and mostly that high when X events are being processed.

--- src/main.c
+++ src/main.c
@@ -10122,7 +10122,13 @@ void UpdateTime( XtPointer clientData, /*@unused@*/ XtIntervalId id ) {
     // completed.  Note:  Setting this to a '1' or '0' can cause
     // some systems (RedHat/FreeBSD) to spin their wheels a lot,
     // using up great amounts of CPU time.
+#ifdef __CYGWIN__
+    // Cygwin performance is abysmal if nexttime is lower than 50, almost
+    // acceptable at 200.
+    nexttime = 200;
+#else
     nexttime = 2;
+#endif // __CYGWIN__
 
 
     if (last_updatetime > sec_now()) {
@@ -11017,15 +11023,6 @@ if (end_critical_section(&data_lock, "main.c:UpdateTime(2)" ) > 0)
 
         check_pointer_position();
     }
-
-
-#ifdef __CYGWIN__
-    // These are needed in order to keep Xastir from using too much
-    // CPU on Cygwin.
-    usleep(2);
-    usleep(2);
-#endif  // __CYGWIN__
-
 
     sched_yield();  // Yield the processor to another thread
 
