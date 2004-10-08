7228f9b2465aea74ecc9f6753dfa1012018276d9
Curt Mills <curt.we7u@gmail.com>
2004-10-08 18:31:44 +0000

    Adding copyright and licensing info.

--- scripts/toporama50k.pl
+++ scripts/toporama50k.pl
@@ -1,7 +1,27 @@
 #!/usr/bin/perl
 
 # $Id$
+
+# Copyright (C) 2004 Adi Linden, VA3ADI.
 #
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# Look at the README for more information on the program.
+
+
 # This will retrieve the 1:50k map images from the "Department of
 # Natural Resources Geomatics Canada" site.  These are topographic
 # map files for the whole of Canada!
