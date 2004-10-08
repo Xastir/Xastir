533d9bf1dd39243973dcc1d2fc02dea9ef9ebfca
Curt Mills <curt.we7u@gmail.com>
2004-10-08 18:33:27 +0000

    Oops.  Changing copyright to correct one.

--- scripts/toporama50k.pl
+++ scripts/toporama50k.pl
@@ -2,7 +2,7 @@
 
 # $Id$
 
-# Copyright (C) 2004 Adi Linden, VA3ADI.
+# Copyright (C) 2004 The Xastir Group.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
