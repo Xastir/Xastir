e50e3b73e8d4811acb0e219b9badc18f43120b3d
Curt Mills <curt.we7u@gmail.com>
2005-01-20 23:40:26 +0000

    Minor efficiency increase for is_my_call() function.  We don't need to check the length of the two strings as strcmp() does that already.

--- src/db.c
+++ src/db.c
@@ -221,18 +221,8 @@ int is_my_call(char *call, int exact) {
     if (exact) {
         // We're looking for an exact match
         ok = (int)( !strcmp(call,my_callsign) );
-        if (ok) {
-            // Ok so far.  Now check that the length of each is the
-            // same.
-            if (strlen(call) != strlen(my_callsign)) {
-                ok = 0; // Not the same!
-            }
-            else {
-//fprintf(stderr,"My exact call found: %s\n",call);
-            }
-        }
+        //fprintf(stderr,"My exact call found: %s\n",call);
     }
-
     else {
         // We're looking for a similar match.  Compare only up to
         // the '-' in each (if present).
