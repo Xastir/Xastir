f869cad0ac0dba06e7ac0d7fc8b514bda37a97df
cvs2git <admin@example.com>
2005-05-09 16:34:06 +0000

    This commit was manufactured by cvs2git to create tag 'xastir150'.
    Sprout from master 2005-05-09 16:34:05 UTC Curt Mills <curt.we7u@gmail.com> 'Updating for soon to be done 1.5.0 release.'
    Cherrypick from master 2004-03-23 22:56:56 UTC Curt Mills <curt.we7u@gmail.com> 'Changes by Bruce Bennett, KB8ROP.':
        src/Davis/AUTHORS
        src/Davis/COPYING
        src/Davis/ChangeLog
        src/Davis/INSTALL
        src/Davis/Makefile.am
        src/Davis/NEWS
        src/Davis/README
        src/Davis/bootstrap.sh
        src/Davis/configure.ac
        src/Davis/src/Makefile.am
        src/Davis/src/db2APRS.c
        src/Davis/src/defs.h
    Cherrypick from master 2004-01-26 16:18:25 UTC Curt Mills <curt.we7u@gmail.com> 'Updating copyright notices.':
        src/Davis/config.h
        src/Davis/db2APRS.c
    Cherrypick from master 2004-09-24 16:49:19 UTC Tom Russo <tom.km5vy@gmail.com> 'Remove DOS line terminators.':
        src/db.h

---
+++ src/Davis/bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# $Id$
+#
+# Copyright (C) 2000-2004  The Xastir Group
+#
+#
+# This simple routine will run autostuff in the appropriate
+# order to generate the needed configure/makefiles
+#
+
+echo "Removing autom4te.cache directory"
+rm -rf autom4te.cache
+
+echo "Running aclocal"
+aclocal
+
+echo "Running autoheader"
+autoheader
+
+echo "Running autoconf"
+autoconf
+
+# Cygwin needs these params to be separate
+echo "Running automake"
+automake -a -c
+
