5a497491e97ac1aefb5288bbecd1acdfce584ec7
Curt Mills <curt.we7u@gmail.com>
2005-09-15 19:38:53 +0000

    Adding the long integer time at the beginning of the timestamp line for the log files.  Makes sorting easy.

--- src/util.c
+++ src/util.c
@@ -2812,11 +2812,19 @@ void log_data(char *file, char *line) {
     else {
         char temp[200];
         char timestring[100+1];
-
+        struct tm *time_now;
+        time_t secs_now;
 
         // Fetch the current date/time string
-        get_timestamp(timestring);
-        xastir_snprintf(temp, sizeof(temp), "# %s", timestring);
+//        get_timestamp(timestring);
+        secs_now=sec_now();
+        time_now = localtime(&secs_now);
+        (void)strftime(timestring,100,"%a %b %d %H:%M:%S %Z %Y",time_now);
+        xastir_snprintf(temp,
+            sizeof(temp),
+            "# %ld  %s",
+            (unsigned long)secs_now,
+            timestring);
 
         // Change back to the base directory
         chdir(get_user_base_dir(""));
