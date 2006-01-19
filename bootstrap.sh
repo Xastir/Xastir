bfcea98537f0b442941a4d66239be5b897cbe193
Curt Mills <curt.we7u@gmail.com>
2006-01-19 20:24:06 +0000

    Fixing a problem in Automake 1.9 (on SuSE 10 at least) where the mkinstalldir script doesn't get copied over by Automake.  We have to copy it over ourselves after we invoke Automake.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,20 +9,39 @@
 # order to generate the needed configure/makefiles
 #
 
-echo "5... Removing autom4te.cache directory"
+echo "6... Removing autom4te.cache directory"
 rm -rf autom4te.cache
 
-echo "4... Running aclocal"
+echo "5... Running aclocal"
 aclocal
 
-echo "3... Running autoheader"
+echo "4... Running autoheader"
 autoheader
 
-echo "2... Running autoconf"
+echo "3... Running autoconf"
 autoconf
 
 # Cygwin needs these params to be separate
-echo "1... Running automake"
+echo "2... Running automake"
 automake -a -c
 
+# Automake-1.9 on SuSE 10 doesn't copy mkinstalldirs.  Check whether
+# it is missing and copy it over ourselves if so.
+if [ -e "mkinstalldirs" ]
+then
+  echo "1... Checking for 'mkinstalldirs': Found!"
+else
+  echo "1... Checking for 'mkinstalldirs': Not Found!"
+  echo "     Copying it from '/usr/share/automake*'"
+  cp /usr/share/automake*/mkinstalldirs .
+  if [ -e "mkinstalldirs" ]
+  then
+    echo "     Checking for 'mkinstalldirs': Found!"
+  else
+    echo "ERROR: Couldn't copy the file."
+    cp /usr/share/automake*/mkinstalldirs .
+  fi
+fi
+
 echo "Bootstrap complete."
+
