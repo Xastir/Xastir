b0585ac7acec1e207afc8623b730c4e8bc6d72c9
Curt Mills <curt.we7u@gmail.com>
2006-01-20 16:52:05 +0000

    Formatting changes to bootstrap.sh.  Added a section to README which details getting rid of an SuSE-10 warning when running bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,36 +9,36 @@
 # order to generate the needed configure/makefiles
 #
 
-echo "6... Removing autom4te.cache directory"
+echo "    6) Removing autom4te.cache directory..."
 rm -rf autom4te.cache
 
-echo "5... Running aclocal"
+echo "    5) Running aclocal..."
 aclocal
 
-echo "4... Running autoheader"
+echo "    4) Running autoheader..."
 autoheader
 
-echo "3... Running autoconf"
+echo "    3) Running autoconf..."
 autoconf
 
 # Cygwin needs these params to be separate
-echo "2... Running automake"
+echo "    2) Running automake..."
 automake -a -c
 
 # Automake-1.9 on SuSE 10 doesn't copy mkinstalldirs.  Check whether
 # it is missing and copy it over ourselves if so.
 if [ -e "mkinstalldirs" ]
 then
-  echo "1... Checking for 'mkinstalldirs': Found!"
+  echo "    1) Checking for 'mkinstalldirs'... Found!"
 else
-  echo "1... Checking for 'mkinstalldirs': Not Found!"
-  echo "     Copying it from '/usr/share/automake*'"
+  echo "    1) Checking for 'mkinstalldirs'... Not Found"
+  echo "       Copying it from '/usr/share/automake*'"
   cp /usr/share/automake*/mkinstalldirs .
   if [ -e "mkinstalldirs" ]
   then
-    echo "     Checking for 'mkinstalldirs': Found!"
+    echo "       Checking for 'mkinstalldirs'... Found!"
   else
-    echo "ERROR: Couldn't copy the file."
+    echo "***ERROR: Couldn't copy the file***"
     cp /usr/share/automake*/mkinstalldirs .
   fi
 fi
