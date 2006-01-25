4086941317b06a20c6411377f116c4ea9358d38e
Jim Chandler <n0vh@users.sourceforge.net>
2006-01-25 19:36:27 +0000

    Support for LaCrosse WX-23xx weather station support

---
+++ LaCrosse/src/defs.h
@@ -0,0 +1,13 @@
+//
+// Copyright (C) 2005 Jim Chandler <jim@n0vh.org>
+// Portions Copyright (C) 2004-2005 The Xastir Group
+//
+//
+// Dummy info here for now...
+//
+
+
+#define VERSION "0.1.0"
+#define PORT	1313
+#define CONNECTIONS	20
+#define NOFILE		20
