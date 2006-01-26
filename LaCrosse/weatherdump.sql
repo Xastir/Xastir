f779b353d6105e5b8cda4136ab8428d8d768bba1
Jim Chandler <n0vh@users.sourceforge.net>
2006-01-26 15:55:41 +0000

    SQL file of open2300 database

---
+++ LaCrosse/weatherdump.sql
@@ -0,0 +1,32 @@
+-- MySQL dump 8.23
+--
+-- Host: localhost    Database: open2300
+---------------------------------------------------------
+-- Server version	3.23.58-log
+
+--
+-- Table structure for table `weather`
+--
+
+CREATE TABLE weather (
+  timestamp bigint(14) NOT NULL default '0',
+  rec_date date NOT NULL default '0000-00-00',
+  rec_time time NOT NULL default '00:00:00',
+  temp_in decimal(3,1) NOT NULL default '0.0',
+  temp_out decimal(3,1) NOT NULL default '0.0',
+  dewpoint decimal(3,1) NOT NULL default '0.0',
+  rel_hum_in tinyint(3) NOT NULL default '0',
+  rel_hum_out tinyint(3) NOT NULL default '0',
+  windspeed decimal(3,1) NOT NULL default '0.0',
+  wind_angle decimal(3,1) NOT NULL default '0.0',
+  wind_direction char(3) NOT NULL default '',
+  wind_chill decimal(3,1) NOT NULL default '0.0',
+  rain_1h decimal(4,2) NOT NULL default '0.00',
+  rain_24h decimal(4,2) NOT NULL default '0.00',
+  rain_total decimal(5,2) NOT NULL default '0.00',
+  rel_pressure decimal(4,2) NOT NULL default '0.00',
+  tendency varchar(7) NOT NULL default '',
+  forecast varchar(6) NOT NULL default '',
+  UNIQUE KEY timestamp (timestamp)
+) TYPE=MyISAM;
+
