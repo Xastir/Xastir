52e459b6482ff2f4656c64a47ae3c9df217dd17c
Curt Mills <curt.we7u@gmail.com>
2006-01-26 16:59:21 +0000

    Updating bootstrap.sh for separate weather apps so they are the same as the main Xastir bootstrap.sh

--- LaCrosse/bootstrap.sh
+++ LaCrosse/bootstrap.sh
@@ -2,26 +2,46 @@
 #
 # $Id$
 #
-# Copyright (C) 2000-2005  The Xastir Group
+# Copyright (C) 2000-2006  The Xastir Group
 #
 #
 # This simple routine will run autostuff in the appropriate
 # order to generate the needed configure/makefiles
 #
 
-echo "Removing autom4te.cache directory"
+echo "    6) Removing autom4te.cache directory..."
 rm -rf autom4te.cache
 
-echo "Running aclocal"
+echo "    5) Running aclocal..."
 aclocal
 
-echo "Running autoheader"
+echo "    4) Running autoheader..."
 autoheader
 
-echo "Running autoconf"
+echo "    3) Running autoconf..."
 autoconf
 
 # Cygwin needs these params to be separate
-echo "Running automake"
+echo "    2) Running automake..."
 automake -a -c
+
+# Automake-1.9 on SuSE 10 doesn't copy mkinstalldirs.  Check whether
+# it is missing and copy it over ourselves if so.
+if [ -e "mkinstalldirs" ]
+then
+  echo "    1) Checking for 'mkinstalldirs'... Found!"
+else
+  echo "    1) Checking for 'mkinstalldirs'... Not Found"
+  echo "       Copying it from '/usr/share/automake*'"
+  cp /usr/share/automake*/mkinstalldirs .
+  if [ -e "mkinstalldirs" ]
+  then
+    echo "       Checking for 'mkinstalldirs'... Found!"
+  else
+    echo "***ERROR: Couldn't copy the file***"
+    cp /usr/share/automake*/mkinstalldirs .
+  fi
+fi
+
+echo "Bootstrap complete."
 
