33f3c92a73075f7c70aa676cfd3512c68a5762eb
Curt Mills <curt.we7u@gmail.com>
2006-01-27 16:30:36 +0000

    Since the three bootstrap.sh scripts are the same anyway, run the one from one directory up instead so that we don't have to keep rev'ing all three of them when a change is needed.  Like, duh!

--- Davis/bootstrap.sh
+++ Davis/bootstrap.sh
@@ -5,51 +5,6 @@
 # Copyright (C) 2000-2006  The Xastir Group
 #
 #
-# This simple routine will run autostuff in the appropriate
-# order to generate the needed configure/makefiles
-#
 
-echo "    6) Removing autom4te.cache directory..."
-rm -rf autom4te.cache
-
-echo "    5) Running aclocal..."
-aclocal
-
-echo "    4) Running autoheader..."
-autoheader
-
-echo "    3) Running autoconf..."
-autoconf
-
-# Cygwin needs these parameters to be separate.
-echo "    2) Running automake..."
-automake -a -c
-
-# Automake-1.9 on SuSE 10 doesn't copy over "mkinstalldirs" if
-# missing.  Check whether it is missing and copy it over ourselves
-# if so.
-if [ -e "mkinstalldirs" ]
-then
-  echo "    1) Checking for 'mkinstalldirs'... Found!"
-else
-  echo "    1) Checking for 'mkinstalldirs'... Not Found"
-  echo "       Attempting to copy it from system directories'"
-  (cp /usr/local/share/automake*/mkinstalldirs . 2>/dev/null)
-  # Did we succeed?
-  if [ $? ]
-  then
-    # Failed the copy above, try in another directory.
-    (cp /usr/share/automake*/mkinstalldirs . 2>/dev/null)
-  fi
-  # Check whether we have the file now in our current directory and
-  # that it is executable.
-  if [ -x "mkinstalldirs" ]
-  then
-    echo "       Checking for 'mkinstalldirs'... Found!"
-  else
-    echo "***ERROR: Couldn't copy the file***"
-  fi
-fi
-
-echo "Bootstrap complete."
+../bootstrap.sh
 
