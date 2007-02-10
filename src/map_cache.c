5cdb31d41d6fbc5ca438d8bf350dbd0bf811ba9d
Curt Mills <curt.we7u@gmail.com>
2007-02-10 03:04:41 +0000

    Commenting out one assignment (not used) which causes a compiler warning on some systems.

--- src/map_cache.c
+++ src/map_cache.c
@@ -240,8 +240,9 @@ int map_cache_put( char * map_cache_url, char * map_cache_file ){
     mc_key.size = sizeof("CACHE_SPACE_USED"); 
 
 // check "CACHE_SPACE_USED" record in db
-	
-    if (((mc_ret = dbp->get(dbp, NULL, &mc_key, &mc_data, 0)) == 0) &&  ( mc_data.data != NULL ) ) {
+
+// mc_ret is assigned here but not used.  Commented it out for now.
+    if (((/* mc_ret = */ dbp->get(dbp, NULL, &mc_key, &mc_data, 0)) == 0) &&  ( mc_data.data != NULL ) ) {
         ( debug_level & 512 ) && fprintf(stderr, "map_cache_put: %s: key retrieved: data was %s.\n",
             (mc_key.data == NULL) ? "(null)" : (char *)mc_key.data,
             (mc_data.data == NULL) ? "(null)" : (char *)mc_data.data);
