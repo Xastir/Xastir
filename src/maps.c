4409d261c10157c4a7c23732f9a190263eb96a82
Gerald Stueve <k4int1@gmail.com>
2007-02-13 20:22:07 +0000

    Make sure all sides of the box are checked, not just three of them. Also make sure if an alert is not on screen clear it off.

--- src/maps.c
+++ src/maps.c
@@ -3517,7 +3517,7 @@ int map_visible_lat_lon (double map_min_y,    // f_bottom_map_boundary
     // The quick rejection algorithm:
     //
     if (map_max_y < f_SE_corner_latitude ) return(0); // map below view
-    if (map_min_x > f_SE_corner_longitude) return(0); // view left of  map
+    if (map_max_x < f_NW_corner_longitude) return(0); // map left of view
     if (map_min_y > f_NW_corner_latitude ) return(0); // view below map
     if (map_min_x > f_SE_corner_longitude) return(0); // view left of  map
 
@@ -5679,6 +5679,7 @@ void draw_map (Widget w, char *dir, char *filenm, alert_entry *alert,
         // See if map is visible.  If not, skip it.
         if (onscreen == MAP_NOT_VIS) {  // Map is not visible, skip it.
             //fprintf(stderr,"map not visible\n");
+	    if (alert) alert->flags[on_screen] = 'N';
             return;
         }
     }
