f27523f80b031816cd84e37b07adf0d059002ce3
Curt Mills <curt.we7u@gmail.com>
2008-01-23 06:00:37 +0000

    Reformatting debug statement so that it won't generate compiler warnings.

--- src/map_cache.c
+++ src/map_cache.c
@@ -243,9 +243,12 @@ int map_cache_put( char * map_cache_url, char * map_cache_file ){
 
 // mc_ret is assigned here but not used.  Commented it out for now.
     if (((/* mc_ret = */ dbp->get(dbp, NULL, &mc_key, &mc_data, 0)) == 0) &&  ( mc_data.data != NULL ) ) {
-        ( debug_level & 512 ) && fprintf(stderr, "map_cache_put: %s: key retrieved: data was %s.\n",
-            (mc_key.data == NULL) ? "(null)" : (char *)mc_key.data,
-            (mc_data.data == NULL) ? "(null)" : (char *)mc_data.data);
+
+        if ( debug_level & 512 ) {
+            fprintf(stderr, "map_cache_put: %s: key retrieved: data was %s.\n",
+                (mc_key.data == NULL) ? "(null)" : (char *)mc_key.data,
+                (mc_data.data == NULL) ? "(null)" : (char *)mc_data.data);
+        }
 
         if (mc_data.data == NULL)
             mc_space_used = 0;
