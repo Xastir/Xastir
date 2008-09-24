140aaf7709443f8b14e37831458573e9576bdac8
Tom Russo <tom.km5vy@gmail.com>
2008-09-24 16:16:42 +0000

    Rename "testawk" to "testdbfawk" and add it to the list of programs that "make install" will install.
    This addresses a couple of issues:
      0) Testawk is a *terrible* name for this program, because in fact it does
         not test "awk", the system program, but rather "dbfawk," the Xastir
         feature inspired by awk but completely unrelated to awk in any other sense.
      1) testawk was never installed by Xastir's build process, meaning that
         packagers of Xastir binaries for various systems did not bundle this
         little program with their binary packages.  There is even a bug in
         debian's bug tracker from an Xastir user asking that the xastir package
         have testawk installed (in a non-standard place because of its
         "generic, even misleading name") by the .deb package.
    
    I've updated README.MAPS and the LSB-BUILD script to make them refer to
    testdbfawk instead of testawk.  I'll still have to change various web resources
    so they point at the new program, too.

--- src/testdbfawk.c
+++ src/testdbfawk.c
@@ -110,7 +110,7 @@ awk_rule rules[] = {
 int nrules = sizeof(rules)/sizeof(rules[0]);
 
 void usage(void) {
-  fprintf(stderr,"Usage: testawk [-f file.awk| -D dir] -d file.dbf\n");
+  fprintf(stderr,"Usage: testdbfawk [-f file.awk| -D dir] -d file.dbf\n");
   fprintf(stderr," -D for dir containing *.dbfawk files.\n");
   fprintf(stderr," or -f for file containing awk rules.\n");
   fprintf(stderr," -d for dbf file to parse \n");
