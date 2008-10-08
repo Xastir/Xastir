6d73c7dcecacca91ad13174515f28e330b2a0d7a
Curt Mills <curt.we7u@gmail.com>
2008-10-08 06:10:29 +0000

    Reverting back to the original code for strncpy() in this module:  The previous change caused a panic.

--- src/shp_hash.c
+++ src/shp_hash.c
@@ -236,9 +236,9 @@ void add_shp_to_hash(char *filename, SHPHandle sHP) {
     temp->filename = (char *) malloc(sizeof(char)*(filenm_len+1));
     CHECKMALLOC(temp->filename);
 
-    //strncpy(temp->filename,filename,filenm_len+1);
-    //temp->filename[filenm_len]='\0';  // just to be safe
-    xastir_snprintf(temp->filename,sizeof(shpinfo),"%s",filename);
+    strncpy(temp->filename,filename,filenm_len+1);
+    temp->filename[filenm_len]='\0';  // just to be safe
+//    xastir_snprintf(temp->filename,sizeof(shpinfo),"%s",filename);
 
     temp->root = Xastir_RTreeNewIndex();  
     temp->creation = sec_now();
