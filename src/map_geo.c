8f6a7b02cc86c20290b3c8ca5c0a7dbcd604ea60
Tom Russo <tom.km5vy@gmail.com>
2008-11-12 23:26:14 +0000

    Comment out the use of GetOnePixel and the variable it assigns to.
    This is for two reasons:
    
     1) F*!#*ng ImageMagick has broken its API yet again with version 6.4.5,
        making GetOnePixel no longer compatible with usage prior to 6.4.4
     2) map_geo.c does this:
          PixelPacket target;
          target=GetOnePixel(image,0,0)
        after which it does NOTHING with the pixel so retrieved.  So commenting
        out this useless line is less work than what I had started doing,
        which was hacking the configure detection of ImageMagick to distinguish
        between pre-6.4.5 and post-6.4.5, and coding around this garbage.
    
    I Hate ImageMagick.

--- src/map_geo.c
+++ src/map_geo.c
@@ -1859,10 +1859,10 @@ fprintf(stderr,"2 ");
     //         lower loops to speed them up... 
     if ( do_crop) {
         int x, y;
-        PixelPacket target;
+        //        PixelPacket target;
         register PixelPacket *q;
-        
-        target=GetOnePixel(image,0,0);
+      
+        //        target=GetOnePixel(image,0,0);
         for (y=0; y < (long) image->rows; y++) {
             q=GetImagePixels(image,0,y,image->columns,1);
             if (q == (PixelPacket *) NULL)
