4d995ce9525af18f6425e9ebd2585c78ddbec7e1
Curt Mills <curt.we7u@gmail.com>
2009-06-29 07:14:51 +0000

    Fixing Canadian callsign lookup to add VO and VY prefixes.

--- src/db.c
+++ src/db.c
@@ -5840,7 +5840,15 @@ begin_critical_section(&db_station_info_lock, "db.c:Station_data" );
         }
 
 
-        // Add FCC button only if probable match
+        // Add FCC button only if probable match.  The U.S. has
+        // these prefixes assigned but not all are used for amateur
+        // stations:
+        //
+        //   AAA-ALZ
+        //   KAA-KZZ
+        //   NAA-NZZ
+        //   WAA-WZZ
+        //
         else if ((! strncmp(local_station,"A",1)) || (!  strncmp(local_station,"K",1)) ||
             (! strncmp(local_station,"N",1)) || (! strncmp(local_station,"W",1))  ) {
 
@@ -5867,8 +5875,18 @@ begin_critical_section(&db_station_info_lock, "db.c:Station_data" );
         }
 
 
-        // Add RAC button only if probable match
-        else if (!strncmp(local_station,"VE",2) || !strncmp(local_station,"VA",2)) {
+        // Add RAC button only if probable match.  Canada has these
+        // prefixes assigned but not all are used for amateur
+        // stations:
+        //
+        //   CFA-CKZ
+        //   CYA-CZZ
+        //   VAA-VGZ
+        //   VOA-VOZ
+        //   VXA-VYZ
+        //   XJA-XOZ
+        //
+        else if (!strncmp(local_station,"VA",2) || !strncmp(local_station,"VE",2) || !strncmp(local_station,"VO",2) || !strncmp(local_station,"VY",2)) {
             button_rac = XtVaCreateManagedWidget(langcode("WPUPSTI004"),xmPushButtonGadgetClass, form,
                             XmNtopAttachment, XmATTACH_NONE,
                             XmNbottomAttachment, XmATTACH_WIDGET,
