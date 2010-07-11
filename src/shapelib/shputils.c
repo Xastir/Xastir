2e637b971426bda840196a18e12dad88add0b1c0
Curt Mills <curt.we7u@gmail.com>
2010-07-11 06:22:55 +0000

    Setting up the rest of shapelib and shapelib/contrib so that everything compiles.  Had to add an include to shputils.c and add a "1" parameter to exit() in one spot.

--- src/shapelib/shputils.c
+++ src/shapelib/shputils.c
@@ -53,6 +53,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.2  2009/06/04 04:25:19  we7u
+ * Getting rid of compiler warnings due to unused variable.
+ *
  * Revision 1.1  2006/11/10 21:48:10  tvrusso
  * Add shapelib as an internal library, and use it if we don't find an external
  * one.
@@ -104,6 +107,8 @@
 
 #include "shapefil.h"
 #include "string.h"
+#include <stdlib.h>
+
 #ifndef FALSE
 #  define FALSE		0
 #  define TRUE		1
@@ -297,7 +302,7 @@ int main( int argc, char ** argv )
                 if (factor == 0)
                 {
                   if (infactor ==0)
-                  { puts("ERROR: Input unit must be defined before output unit"); exit(); }
+                  { puts("ERROR: Input unit must be defined before output unit"); exit(1); }
                   factor=infactor/outfactor;
                 }
                 printf("Output file coordinate values will be factored by %lg\n",factor);
