274b524847d5f92ee2238e5c999fd0c0b3d0a854
Tom Russo <tom.km5vy@gmail.com>
2011-09-05 17:54:37 +0000

    Add compress=packbits to geotiff creation options for minor disk space savings.

--- scripts/geopdf2gtiff.pl
+++ scripts/geopdf2gtiff.pl
@@ -134,7 +134,8 @@ $outputTif =~ s/pdf/tif/i;
 # We will warp from whatever the coordinate system is into EPSG:4326, the
 # coordinate system that requires the least work and involves the fewest
 # approximations from Xastir.
-$theGdalWarp="gdalwarp -cutline $inputPDF.vrt -crop_to_cutline -t_srs EPSG:4326 $inputPDF $outputTif";
+$theGdalWarp="gdalwarp -cutline $inputPDF.vrt -crop_to_cutline -t_srs EPSG:4326  -co \"COMPRESS=PACKBITS\" $inputPDF $outputTif";
+#$theGdalWarp="gdalwarp -cutline $inputPDF.vrt -crop_to_cutline -t_srs EPSG:4326  $inputPDF $outputTif";
 
 system ($theGdalWarp) == 0 or die "System $theGdalWarp failed: $?";
 
@@ -143,9 +144,10 @@ if ($#bandinfo>0)
 {
     print "This is a multi-band raster, dithering...\n";
     system ("mv $outputTif $$.tif");
-    $theRGB2PCT="rgb2pct.py $$.tif $outputTif";
+    $theRGB2PCT="rgb2pct.py $$.tif $$-2.tif";
     system ($theRGB2PCT) == 0 or die "Could not run rgb2pct: $?";
-    system("rm  $$.tif");
+    system("gdal_translate -co \"COMPRESS=PACKBITS\" $$-2.tif $outputTif") == 0 or die "Could not gdal_translate: $?";
+    system("rm  $$.tif $$-2.tif");
 }
 
 # now clean up our mess:
