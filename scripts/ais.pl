c966ada57c493f93eca11b2c94f031b689cc73ff
Curt Mills <curt.we7u@gmail.com>
2016-03-21 14:05:58 +0000

    Correcting the remote Xastir ssid logic. Adding placeholders for a several more AIS message types. Printing out message type to console.

--- scripts/ais.pl
+++ scripts/ais.pl
@@ -33,7 +33,10 @@
 #
 #
 # Good AIS info:
-# http://catb.org/gpsd/AIVDM.html
+#   http://catb.org/gpsd/AIVDM.html
+#
+# Example packets w/decoding of fields:
+#   http://fossies.org/linux/gpsd/test/sample.aivdm
 #
 #
 # TODO: Decode more than just messages types 1/2/3.
@@ -111,11 +114,9 @@ if ($result =~ m/NACK/) {
 chomp($remote_call);
 $remote_call =~ s/Received:\s+//;
 
-$remote_ssid = 0;
-if (defined ($remote_ssid) && $remote_ssid ne "") {
-    chomp($remote_ssid);
-    if ($remote_ssid eq "") { $remote_ssid = 0; }
-}
+if ( !defined($remote_ssid) ) { $remote_ssid = ""; }
+if ($remote_ssid ne "") { chomp($remote_ssid); }
+if ($remote_ssid eq "") { $remote_ssid = 0; }
 
 #print "$remote_call $remote_ssid $injection_call $injection_ssid\n";
 #if ($remote_call eq $injection_call) { print "Call matches\n"; }
@@ -163,6 +164,19 @@ while ($server->recv($received_data, 1024)) {
     #$received_data = "!AIVDM,1,1,,B,15Mf@6P001G?\@vdKFC:ckaD@0<26,0*26";   # Message Type 1
     #$received_data = "!AIVDM,1,1,,A,15MToB0w3?o?MG8KEswnQU:@0h4S,0*1A";    # Message Type 1
     #$received_data = "!AIVDM,1,1,,B,15Mf@6P001G?\@v>KFC5;kaD@084M,0*52";   # Message Type 1
+    #$received_data = "!AIVDM,1,1,,B,25Cjtd0Oj;Jp7ilG7=UkKBoB0<06,0*60";    # Message Type 2
+    #$received_data = "!AIVDM,1,1,,A,38Id705000rRVJhE7cl9n;160000,0*40";    # Message Type 3
+    #$received_data = "!AIVDM,2,1,1,A,55?MbV02;H;s<HtKR20EHE:0\@T4\@Dn2222222216L961O5Gf0NSQEp6ClRp8,0*1C";   # Message Type 5
+    #$received_data = "!AIVDM,1,1,,A,91b77=h3h00nHt0Q3r@@07000<0b,0*69";    # Message Type 9
+    #$received_data = "!AIVDM,1,1,,B,91b55wi;hbOS@OdQAC062Ch2089h,0*30";    # Message Type 9
+    #$received_data = "!AIVDM,1,1,,A,B52K>;h00Fc>jpUlNV@ikwpUoP06,0*4C";    # Message Type 18 
+    #$received_data = "!AIVDM,1,1,,A,B52KB8h006fu`Q6:g1McCwb5oP06,0*00";    # Message Type 18 
+    #$received_data = "!AIVDM,1,1,,B,B5O6hr00<veEKmUaMFdEow`UWP06,0*4F";    # Message Type 18 
+    #$received_data = "!AIVDM,1,1,,B,C5N3SRgPEnJGEBT>NhWAwwo862PaLELTBJ:V00000000S0D:R220,0*0B";    # Message Type 19
+    #$received_data = "!AIVDM,1,1,,A,H42O55i18tMET00000000000000,2*6D";     # Message Type 24
+    #$received_data = "!AIVDM,1,1,,A,H42O55lti4hhhilD3nink000?050,0*40";    # Message Type 24
+    #$received_data = "!AIVDM,1,1,,A,KCQ9r=hrFUnH7P00,0*41";                # Message Type 27
+    #$received_data = "!AIVDM,1,1,,B,KC5E2b@U19PFdLbMuc5=ROv62<7m,0*16";    # Message Type 27
 
     chomp($received_data);
     @pieces = split( ',', $received_data);
@@ -221,45 +235,87 @@ while ($server->recv($received_data, 1024)) {
 
     $bmessage_type = substr($bin_string, 0, 6);
     $message_type = &bin2dec($bmessage_type);
-    #print "Message Type: $message_type\n";
+    print "Message Type: $message_type\n";
     # If not messages types 1, 2, 3, skip for now
-    if ($message_type > 3) {
+    if (    $message_type !=  1
+         && $message_type !=  2
+         && $message_type !=  3
+         && $message_type !=  5
+         && $message_type !=  9
+         && $message_type != 18
+         && $message_type != 19
+         && $message_type != 24
+         && $message_type != 27
+                                 ) {
+        # Not something we decode so don't process the sentence.
         next;
     }
 
-    #$brepeat_indicator = substr($bin_string, 6, 2);
-    $buserID = substr($bin_string, 8, 30);
-    #$bnav_status = substr($bin_string, 38, 4);
-    #$brateOfTurn = substr($bin_string, 42, 8);
-    $bspeedOverGnd = substr($bin_string, 50, 10);
-    #$bpositionAccuracy = substr($bin_string, 60, 1);
-    $blongitude = substr($bin_string, 61, 28);
-    $blatitude = substr($bin_string, 89, 27);
-    $bcourseOverGnd = substr($bin_string, 116, 12);
-    #$btrueHeading = substr($bin_string, 128, 9);
-    #$btimestamp = substr($bin_string, 137, 6);
-    #$bregional = substr($bin_string, 143, 2);
-    #$bspare = substr($bin_string, 145, 3);
-    #$bRAIM = substr($bin_string, 148, 1);
-    #$bSOTDMAsyncState = substr($bin_string, 149, 3);
-    #$bSOTDMAslotTimeout = substr($bin_string, 151, 3);
-    #$bSOTDMAslotOffset = substr($bin_string, 154, 14);
+    if (    $message_type ==  1
+         || $message_type ==  2
+         || $message_type ==  3 ) {
+        &process_types_1_2_3($message_type);
+    }
+    elsif ( $message_type == 5 ) {
+        &process_type_5($message_type);
+    }
+    elsif ( $message_type == 9 ) {
+        &process_type_9($message_type);
+    }
+    elsif ( $message_type == 18 ) {
+        &process_type_18($message_type);
+    }
+    elsif ( $message_type == 19 ) {
+        &process_type_19($message_type);
+    }
+    elsif ( $message_type == 24 ) {
+        &process_type_24($message_type);
+    }
+    elsif ( $message_type == 27 ) {
+        &process_type_27($message_type);
+    }
+   
+} # End of main processing loop
 
-    $userID = &bin2dec($buserID);
+
+
+# Message types 1, 2 and 3: Position Report Class A
+#
+sub process_types_1_2_3() {
+    #my $brepeat_indicator = substr($bin_string, 6, 2);
+    my $buserID = substr($bin_string, 8, 30);
+    #my $bnav_status = substr($bin_string, 38, 4);
+    #my $brateOfTurn = substr($bin_string, 42, 8);
+    my $bspeedOverGnd = substr($bin_string, 50, 10);
+    #my $bpositionAccuracy = substr($bin_string, 60, 1);
+    my $blongitude = substr($bin_string, 61, 28);
+    my $blatitude = substr($bin_string, 89, 27);
+    my $bcourseOverGnd = substr($bin_string, 116, 12);
+    #my $btrueHeading = substr($bin_string, 128, 9);
+    #my $btimestamp = substr($bin_string, 137, 6);
+    #my $bregional = substr($bin_string, 143, 2);
+    #my $bspare = substr($bin_string, 145, 3);
+    #my $bRAIM = substr($bin_string, 148, 1);
+    #my $bSOTDMAsyncState = substr($bin_string, 149, 3);
+    #my $bSOTDMAslotTimeout = substr($bin_string, 151, 3);
+    #my $bSOTDMAslotOffset = substr($bin_string, 154, 14);
+
+    my $userID = &bin2dec($buserID);
     print "User ID: $userID\n";
 
-    $courseOverGnd = &bin2dec($bcourseOverGnd) / 10 ;
+    my $courseOverGnd = &bin2dec($bcourseOverGnd) / 10 ;
     if ($courseOverGnd == 0) {
         $courseOverGnd = 360;
     }
     print "Course: $courseOverGnd\n";
-    $course = sprintf("%03d", $courseOverGnd);
+    my $course = sprintf("%03d", $courseOverGnd);
 
-    $speedOverGnd = &bin2dec($bspeedOverGnd) / 10;
+    my $speedOverGnd = &bin2dec($bspeedOverGnd) / 10;
     print "Speed: $speedOverGnd\n";
-    $speed = sprintf("%03d", $speedOverGnd);
+    my $speed = sprintf("%03d", $speedOverGnd);
 
-    $latitude = &signedBin2dec($blatitude) / 600000.0;
+    my $latitude = &signedBin2dec($blatitude) / 600000.0;
+    my $NS;
     print "Latitude: $latitude\n";
     if ($latitude >= 0.0) {
         $NS = 'N';
@@ -267,12 +323,13 @@ while ($server->recv($received_data, 1024)) {
         $NS = 'S';
         $latitude = abs($latitude);
     }
-    $latdeg = int($latitude);
-    $latmins = $latitude - $latdeg;
-    $latmins2 = $latmins * 60.0;
-    $lat = sprintf("%02d%05.2f%s", $latdeg, $latmins2, $NS);
+    my $latdeg = int($latitude);
+    my $latmins = $latitude - $latdeg;
+    my $latmins2 = $latmins * 60.0;
+    my $lat = sprintf("%02d%05.2f%s", $latdeg, $latmins2, $NS);
 
-    $longitude = &signedBin2dec($blongitude) / 600000.0;
+    my $longitude = &signedBin2dec($blongitude) / 600000.0;
+    my $EW;
     print "Longitude: $longitude\n";
     if ($longitude >= 0.0) {
         $EW = 'E';
@@ -280,23 +337,239 @@ while ($server->recv($received_data, 1024)) {
         $EW = 'W';
         $longitude = abs($longitude);
     }
-    $londeg = int($longitude);
-    $lonmins = $longitude - $londeg;
-    $lonmins2 = $lonmins * 60.0;
-    $lon = sprintf("%03d%05.2f%s", $londeg, $lonmins2, $EW);
+    my $londeg = int($longitude);
+    my $lonmins = $longitude - $londeg;
+    my $lonmins2 = $lonmins * 60.0;
+    my $lon = sprintf("%03d%05.2f%s", $londeg, $lonmins2, $EW);
 
     print "\n";
 
-    $symbol = "s";
-    $aprs="$xastir_user>APRS:)$userID!$lat/$lon$symbol$course/$speed";
+    my $symbol = "s";
+    my $aprs="$xastir_user>APRS:)$userID!$lat/$lon$symbol$course/$speed";
+    print "$aprs\n";
+    # xastir_udp_client  <hostname> <port> <callsign> <passcode> {-identify | [-to_rf] <message>}
+    my $result = `$udp_client $xastir_host $xastir_port $xastir_user $xastir_pass \"$aprs\"`;
+    if ($result =~ m/NACK/) {
+        die "Received NACK from Xastir: Callsign/Passcode don't match?\n";
+    }
+        return();
+}
+
+
+
+# Message type 5: Static and Voyage Related Data (Vessel name, callsign, ship type)
+#
+sub process_type_5() {
+    # substr($bin_string,   0,   6); # Message Type
+    # my $brepeat_indicator = substr($bin_string,   6,   2); # Repeat Indicator
+    my $buserID = substr($bin_string,   8,  30); # MMSI
+    # substr($bin_string,  38,   2); # AIS Version
+    # substr($bin_string,  40,  30); # IMO Number
+    # substr($bin_string,  70,  42); # Call Sign
+    my $bvessel_name = substr($bin_string, 112, 120); # Vessel Name
+    # substr($bin_string, 232,   8); # Ship Type
+    # substr($bin_string, 240,   9); # Dimension to Bow
+    # substr($bin_string, 249,   9); # Dimension to Stern
+    # substr($bin_string, 258,   6); # Dimension to Port
+    # substr($bin_string, 264,   6); # Dimension to Starboard
+    # substr($bin_string, 270,   4); # Position Fix Type
+    # substr($bin_string, 274,   4); # ETA month (UTC)
+    # substr($bin_string, 278,   5); # ETA day (UTC)
+    # substr($bin_string, 283,   5); # ETA hour (UC)
+    # substr($bin_string, 288,   6); # ETA minute (UTC)
+    # substr($bin_string, 294,   8); # Draught
+    # substr($bin_string, 302, 120); # Destination
+    # substr($bin_string, 422,   1); # DTE
+    # substr($bin_string, 423,   1); # Spare
+
+    my $userID = &bin2dec($buserID);
+    print "User ID: $userID\n";
+
+    my $vessel_name = &bin2text($bvessel_name);
+    print "Vessel Name: $vessel_name\n";
+
+    # Assign tactical call = $vessel_name
+    # Max tactical call in Xastir is 57 chars (56 + terminator?)
+    #
+    $aprs = $xastir_user . '>' . "APRS::TACTICAL :" . $userID . "=" . $vessel_name;
     print "$aprs\n";
     # xastir_udp_client  <hostname> <port> <callsign> <passcode> {-identify | [-to_rf] <message>}
     $result = `$udp_client $xastir_host $xastir_port $xastir_user $xastir_pass \"$aprs\"`;
     if ($result =~ m/NACK/) {
         die "Received NACK from Xastir: Callsign/Passcode don't match?\n";
     }
-} # End of main processing loop
 
+    return();
+}
+
+
+
+# Message type 9: Standard SAR Aircraft Position Report
+#
+sub process_type_9() {
+    # substr($bin_string,   0,   6); # Message Type
+    # my $brepeat_indicator = substr($bin_string,   6,   2); # Repeat Indicator
+    my $buserID = substr($bin_string,   8,  30); # MMSI
+    # substr($bin_string,  38,  12); # Altitude
+    # substr($bin_string,  50,  10); # SOG
+    # substr($bin_string,  60,   1); # Position Accuracy
+    # substr($bin_string,  61,  28); # Longitude
+    # substr($bin_string,  89,  27); # Latitude
+    # substr($bin_string, 116,  12); # Course Over Ground
+    # substr($bin_string, 128,   6); # Time Stamp
+    # substr($bin_string, 134,   8); # Regional reserved
+    # substr($bin_string, 142,   1); # DTE
+    # substr($bin_string, 143,   3); # Spare
+    # substr($bin_string, 146,   1); # Assigned
+    # substr($bin_string, 147,   1); # RAIM flag
+    # substr($bin_string, 148,  20); # Radio status
+
+    return();
+}
+
+
+
+# Message type 18: Standard Class B CS Position Report
+#
+sub process_type_18() {
+    # substr($bin_string,   0,   6); # Message Type
+    # my $brepeat_indicator = substr($bin_string,   6,   2); # Repeat Indicator
+    my $buserID = substr($bin_string,   8,  30); # MMSI
+    # substr($bin_string,  38,   8); # Regional Reserved
+    # substr($bin_string,  46,  10); # Speed Over Ground
+    # substr($bin_string,  56,   1); # Position Accuracy
+    # substr($bin_string,  57,  28); # Longitude
+    # substr($bin_string,  85,  27); # Latitude
+    # substr($bin_string, 112,  12); # Course Over Ground
+    # substr($bin_string, 124,   9); # True Heading
+    # substr($bin_string, 133,   6); # Time Stamp
+    # substr($bin_string, 139,   2); # Regional reserved
+    # substr($bin_string, 141,   1); # CS Unit
+    # substr($bin_string, 142,   1); # Display flag
+    # substr($bin_string, 143,   1); # DSC Flag
+    # substr($bin_string, 144,   1); # Band flag
+    # substr($bin_string, 145,   1); # Message 22 flag
+    # substr($bin_string, 146,   1); # Assigned
+    # substr($bin_string, 147,   1); # RAIM flag
+    # substr($bin_string, 148,  20); # Radio status
+ 
+    return();
+}
+
+
+
+# Message type 19: Extended Class B CS Position Report
+#
+sub process_type_19() {
+    # substr($bin_string,   0,   6); # Message Type
+    # my $brepeat_indicator = substr($bin_string,   6,   2); # Repeat Indicator
+    my $buserID = substr($bin_string,   8,  30); # MMSI
+    # substr($bin_string,  38,   8); # Regional Reserved
+    # substr($bin_string,  46,  10); # Speed Over Ground
+    # substr($bin_string,  56,   1); # Position Accuracy
+    # substr($bin_string,  57,  28); # Longitude
+    # substr($bin_string,  85,  27); # Latitude
+    # substr($bin_string, 112,  12); # Course Over Ground
+    # substr($bin_string, 124,   9); # True Heading
+    # substr($bin_string, 133,   6); # Time Stamp
+    # substr($bin_string, 139,   4); # Regional reserved
+    # substr($bin_string, 143, 120); # Name
+    # substr($bin_string, 263,   8); # Type of ship and cargo
+    # substr($bin_string, 271,   9); # Dimension to Bow
+    # substr($bin_string, 280,   9); # Dimension to Stern
+    # substr($bin_string, 289,   6); # Dimension to Port
+    # substr($bin_string, 295,   6); # Dimension to Starboard
+    # substr($bin_string, 301,   4); # Position Fix Type
+    # substr($bin_string, 305,   1); # RAIM flag
+    # substr($bin_string, 306,   1); # DTE
+    # substr($bin_string, 307,   1); # Assigned mode flag
+    # substr($bin_string, 308,   4); # Spare
+ 
+    return();
+}
+
+
+
+# Message type 24: Static Data Report (Vessel name, callsign, ship type)
+#
+sub process_type_24() {
+    # substr($bin_string,   0,   6); # Message Type
+    # my $brepeat_indicator = substr($bin_string,   6,   2); # Repeat Indicator
+    my $buserID = substr($bin_string,   8,  30); # MMSI
+    # substr($bin_string,  38,   2); # Part Number
+    # substr($bin_string,  40, 120); # Vessel Name
+    # substr($bin_string, 160,   8); # Spare.
+
+# Alternate format starting at bit 40:
+    # substr($bin_string,  40,   8); # Ship Type
+    # substr($bin_string,  48,  18); # Vendor ID
+    # substr($bin_string,  66,   4); # Unit Model Code
+    # substr($bin_string,  70,  20); # Serial Number
+    # substr($bin_string,  90,  42); # Call Sign
+    # substr($bin_string, 132,   9); # Dimension to bow
+    # substr($bin_string, 141,   9); # Dimension to Stern
+    # substr($bin_string, 150,   6); # Dimension to Port
+    # substr($bin_string, 156,   6); # Dimension to Starboard
+
+# Alternate format starting at bit 132:
+    # substr($bin_string, 132,  30); # Mothership MMSI.
+    # substr($bin_string, 162,   6); # Spare
+
+# If the part number field is 0, the rest of message is interpreted as a Part A message.
+# If it is a 1, the rest is interpreted as a Part B message.
+# Interpretation of 30 bits starting at 132: If MMSI is that of an auxiliary craft,
+# then it is the MMSI of the mother ship. Else these 30 bits describe vessel dimensions.
+# Auxiliary craft: MMSI of form 98XXXYYYY, the XXX digits are the country code.
+ 
+    return();
+}
+
+
+
+# Message type 27: Position Report For Long-Range Applications (rare)
+#
+sub process_type_27() {
+    # substr($bin_string,   0,   6); # Message Type
+    # my $brepeat_indicator = substr($bin_string,   6,   2); # Repeat Indicator
+    my $buserID = substr($bin_string,   8,  30); # MMSI
+    # substr($bin_string,  38,   1); # Position Accuracy
+    # substr($bin_string,  39,   1); # RAIM flag
+    # substr($bin_string,  40,   4); # Navigation Status
+    # substr($bin_string,  44,  18); # Longitude
+    # substr($bin_string,  62,  17); # Latitude
+    # substr($bin_string,  79,   6); # Speed Over Ground
+    # substr($bin_string,  85,   9); # Course Over Ground
+    # substr($bin_string,  94,   1); # GNSS Position Status
+    # substr($bin_string,  95,   1); # SPare
+ 
+    return();
+}
+
+ 
+
+# Convert a 6-digit binary string to ASCII text
+# Encoded AIS data: Each 6 bits represents one ASCII character.
+#
+# Each six-bit nibble maps to an ASCII character.
+# Decimal 0-31 map to "@" ( ASCII 64) through "\_" (ASCII 95)
+# Decimal 32-63 map to " " (ASCII 32) though "?" (ASCII 63)
+# Lowercase, backtick, right/left braces, pipe, tilde and DEL can't be encoded
+sub bin2text() {
+    my $input = shift;
+    my $final_string = "";
+    my $len = length($input);
+    my $i;
+    for ($i = 0; $i < $len; $i+=6) {
+        my $binary_char = substr($input, $i, 6);
+        # Convert from binary to decimal (ord)
+        my $d = unpack("N", pack("B32", substr("0" x 32 . $binary_char, -32)));
+        if ($d < 32) { $d += 64; }
+        $final_string = $final_string . chr($d);
+    }
+    $final_string =~ s/\s\s/ /g;
+    return($final_string);
+}
+ 
 
 
 # Convert a signed binary string of ASCII 0's and 1's to a decimal
