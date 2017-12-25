b929560e8fee8afe8be6feb63a1420272707be97
Tom Russo <russo@bogodyn.org>
2017-12-25 11:55:02 -0700

    Unbreak build after IPv6 UDP merge
    The code I just merged to resolve pull request #16 introduced a new compilation
    failure on some versions of Linux.  The code used AI_DEFAULT, which is
    documented in the getipnodebyname man page as being valid, but which is
    apparently NOT defined in a number of linux platform's netdb.h.
    
    The linux systems that don't have AI_DEFAULT defined include ubuntu 14.04
    (which I run on my laptop), and at least one version of SUSE (on which Curt
    runs a Jenkins build to test that Xastir continues to compile after pushes).

--- src/xastir_udp_client.c
+++ src/xastir_udp_client.c
@@ -104,6 +104,10 @@ int try_exchange(struct addrinfo *addr, char *buffer, int buflen)
     return 1;
 }
 
+#ifndef AI_DEFAULT
+#define AI_DEFAULT (AI_V4MAPPED|AI_ADDRCONFIG)
+#endif
+
 // Loop through the possible addresses for hostname (probably IPv6 and IPv4)
 // Tries until we are successful (get a reponse) or we run out of addresses
 int exchange_packet(char *hostname, char *port, char *buffer, int buflen)
