Xastir/Xastir

commit 374e8cf34157a1ac4749ef346f63ec38999f3d40

Added station ID in the form of large text across the screen, and spoken station ID via festival.  Occurs every 9.5 minutes if two new variables in the config file are enabled.

src/xa_config.c
@@ -588,6 +588,7 @@ void save_data(void)  {
 
 #ifdef HAVE_FESTIVAL
             /* Festival speech settings */
+        store_int (fout, "SPEAK_ID",festival_speak_ID);
         store_int (fout, "SPEAK_NEW_STATION",festival_speak_new_station);
         store_int (fout, "SPEAK_PROXIMITY_ALERT",festival_speak_proximity_alert);
         store_int (fout, "SPEAK_TRACKED_ALERT",festival_speak_tracked_proximity_alert);
@@ -595,8 +596,10 @@ void save_data(void)  {
         store_int (fout, "SPEAK_MESSAGE_ALERT",festival_speak_new_message_alert);
         store_int (fout, "SPEAK_MESSAGE_BODY",festival_speak_new_message_body);
         store_int (fout, "SPEAK_WEATHER_ALERT",festival_speak_new_weather_alert);
+        store_int (fout, "SPEAK_ID",festival_speak_ID);
 #endif
-        
+        store_int (fout, "ATV_SCREEN_ID", ATV_screen_ID);
+       
         /* defaults */
         store_long (fout, "DEFAULT_STATION_OLD", (long)sec_old);
 
@@ -1220,8 +1223,12 @@ void load_data_or_default(void) {
     if (!get_int ("SPEAK_WEATHER_ALERT",&festival_speak_new_weather_alert,0,1,0))
         festival_speak_new_weather_alert = 0; 
 
+    if (!get_int ("SPEAK_ID",&festival_speak_ID,0,1,0))
+        festival_speak_new_station = 0;
 #endif
-
+    if (!get_int ("ATV_SCREEN_ID",&ATV_screen_ID,0,1,0))
+        ATV_screen_ID = 0; 
+ 
     /* defaults */
     if (!get_long ("DEFAULT_STATION_OLD", (long *)&sec_old, 1l, 604800l, 4800l))
         sec_old = (time_t)4800l;

config/language-Dutch.sys
@@ -271,7 +271,7 @@ BBARSTA036|HSP vrijmaken, TNC data ophalen..||
 BBARSTA037|GPS tekst gedecodeerd||
 BBARSTA038|Positie verandering bij mijn station||
 BBARSTA039|Indexing %s||
-BBARSTA040|This is Amateur Radio Station %s||
+BBARSTA040|Amateur APRS Station %s||
 BBARSTA041|Waiting for GPS data..||
 #
 # PopUp "View - Incoming Packet Data"

config/language-English.sys
@@ -266,7 +266,7 @@ BBARSTA036|Clearing AUX getting TNC data..||
 BBARSTA037|GPS Data Complete||
 BBARSTA038|Position change on my station||
 BBARSTA039|Indexing %s||
-BBARSTA040|This is Amateur Radio Station %s||
+BBARSTA040|Amateur APRS Station %s||
 BBARSTA041|Waiting for GPS data..||
 #
 # PopUp "View - Incoming Packet Data"

config/language-French.sys
@@ -266,7 +266,7 @@ BBARSTA036|Fermeture port GPS réception donnée TNC..||
 BBARSTA037|GPS météo décodées||
 BBARSTA038|Placez le changement sur ma station||
 BBARSTA039|Indexing %s||
-BBARSTA040|This is Amateur Radio Station %s||
+BBARSTA040|Amateur APRS Station %s||
 BBARSTA041|Waiting for GPS data..||
 #
 # PopUp "View - Incoming Packet Data"

config/language-German.sys
@@ -272,7 +272,7 @@ BBARSTA036|Setze AUX auf TNC Daten..||
 BBARSTA037|Dekodiere GPS daten||
 BBARSTA038|Bringen Sie Änderung auf meine Station in Position||
 BBARSTA039|Indexing %s||
-BBARSTA040|This is Amateur Radio Station %s||
+BBARSTA040|Amateur APRS Station %s||
 BBARSTA041|Waiting for GPS data..||
 #
 # PopUp "Zeige - Packet Radio"

config/language-Italian.sys
@@ -267,7 +267,7 @@ BBARSTA036|Libero AUX, ricezione dati da TNC..||
 BBARSTA037|Dati GPS rilevati||
 BBARSTA038|Posizionare il cambiamento sulla mia stazione||
 BBARSTA039|Indexing %s||
-BBARSTA040|This is Amateur Radio Station %s||
+BBARSTA040|Amateur APRS Station %s||
 BBARSTA041|Waiting for GPS data..||
 #
 #Visualizzazione dati packet

config/language-Portuguese.sys
@@ -265,7 +265,7 @@ BBARSTA036|Limpando AUX para obter dados do TNC..||
 BBARSTA037|Dados do GPS completos||
 BBARSTA038|Posição alterada na minha estação||
 BBARSTA039|Indexando %s||
-BBARSTA040|É uma estação do radioamador %s||
+BBARSTA040|Amateur APRS Station %s||
 BBARSTA041|Waiting for GPS data..||
 #
 # Visualização do tráfego de packet

config/language-Spanish.sys
@@ -267,7 +267,7 @@ BBARSTA036|Limpiando el AUX datos obtenido del TNC..||
 BBARSTA037|Datos de GPS Decondificado||
 BBARSTA038|Coloque el cambio en mi estación||
 BBARSTA039|Indexing %s||
-BBARSTA040|This is Amateur Radio Station %s||
+BBARSTA040|Amateur APRS Station %s||
 BBARSTA041|Waiting for GPS data..||
 #
 # Despliegue Paquete de Datos

commit 63db286f40d0bd8513d52517f552089b9bf9ce63

Fix ancient error in festival SPEAK_ID handling
Ever since it was first introduced in commit 374e8cf34 in 2002, there
was a cut-and-paste error in the code that introduced the ability to
speak an ID through festival.

This error was just reported by Mike Finch <finchm@swva.net> who found
the exact line where the code was wrong.  While the code was correctly
extracting a configuration option from the .cnf file, it was setting
the wrong boolean.

src/xa_config.c
@@ -2415,7 +2415,7 @@ void load_data_or_default(void) {
     festival_speak_new_message_alert = get_int ("SPEAK_MESSAGE_ALERT",0,1,0);
     festival_speak_new_message_body = get_int ("SPEAK_MESSAGE_BODY",0,1,0);
     festival_speak_new_weather_alert = get_int ("SPEAK_WEATHER_ALERT",0,1,0);
-    festival_speak_new_station = get_int ("SPEAK_ID",0,1,0);
+    festival_speak_ID = get_int ("SPEAK_ID",0,1,0);
 #endif  // HAVE_FESTIVAL
 
     ATV_screen_ID = get_int ("ATV_SCREEN_ID",0,1,0);