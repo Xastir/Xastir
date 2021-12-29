2056f51cc1f2a31255551fb92b9da3b8ddb60fde
Tom Russo <russo@bogodyn.org>
2021-12-29 10:44:11 -0700

    Remove derived language generation file from boostrap
    In commit 21ebe38, Curt added generation of derived language
    files (mostly joke languages like Pig Latin and Pirate English) to
    bootstrap.sh, and slightly tweaked that technique the next day in
    commit 8d52f59.
    
    Two years later, in commits aae6102 and 67ed45c, Gerald Stueve modified
    config/Makefile.am to build these language files at build time instead
    of at bootstrap time, which is a better approach.  Unfortunately, this
    was done with little fanfare *and* Gerald did not remove the redundant
    regeneration in bootstrap.sh.
    
    The presence of non-autoconf stuff in bootstrap.sh was weird, so it
    was right to move it elsewhere.  Now, bootstrap.sh is not much
    different (or at all different) from using "autoreconf", the newer,
    less kludgy way of "bootstrapping" that replaced the shell script in
    many projects.
    
    This came up during an attempt to get FreeBSD's Xastir port updated to
    2.1.8.  The new need to run bootstrap.sh even when using tarballs from
    github complicated the port and made it a mess.  But using autoreconf
    is easy.
    
    Fortunately, the generation of language-PirateEnglish.sys and its ilk
    was already being taken care of at "make" time, so the port maintainer
    chose to skip the mess of using bootstrap and just set up the port to
    use autoreconf.
    
    This commit removes the language generation from bootstrap, which
    should have been done at the same time that capability was put into
    config/Makefile.am.
    
    https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=259669

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#/bin/sh -e
 #
 #
 # Copyright (C) 2000-2019 The Xastir Group
@@ -8,34 +8,21 @@
 # order to generate the needed configure/makefiles
 #
 
-echo "    6) Removing autom4te.cache directory..."
+echo "    5) Removing autom4te.cache directory..."
 rm -rf autom4te.cache
 
-echo "    5) Running aclocal..."
+echo "    4) Running aclocal..."
 aclocal
 
-echo "    4) Running autoheader..."
+echo "    3) Running autoheader..."
 autoheader
 
-echo "    3) Running autoconf..."
+echo "    2) Running autoconf..."
 autoconf
 
 # Cygwin needs these parameters to be separate.
-echo "    2) Running automake..."
+echo "    1) Running automake..."
 automake -a -c
-
-# Generate derived language files
-echo -n "    1) Generating derived language files"
-(cd config; ../scripts/langPigLatin.pl -split <language-English.sys >language-PigLatin.sys) 
-echo -n "."
-(cd config; ../scripts/langElmerFudd.pl -split <language-English.sys >language-ElmerFudd.sys) 
-echo -n "."
-(cd config; ../scripts/langMuppetsChef.pl -split <language-English.sys >language-MuppetsChef.sys) 
-echo -n "."
-(cd config; ../scripts/langOldeEnglish.pl -split <language-English.sys >language-OldeEnglish.sys) 
-echo -n "."
-(cd config; ../scripts/langPirateEnglish.pl -split <language-English.sys >language-PirateEnglish.sys) 
-echo "."
 
 echo "Bootstrap complete."
 
