e3e3ae60406a36f01625aa1080ee24a4558a03be
Tom Russo <russo@bogodyn.org>
2025-11-23 10:32:47 -0700

    Start adding a unit test suite for util.c
    util.c was a show stopper for unit testing for a while because it
    pulled in so many other dependencies.  Some of my work in prior PRs
    reduced the interdependence of util.c enough (mostly by removing the
    need for too many enormous include files like main.h) that it is now
    possible to unit test *some* of the utilities.
    
    The functions tested here are just convert_(lat|lon)_(l2s|s2l), which
    are basic functions that convert between latitude/longitude strings
    and long-int Xastir coordinates.  I will be needing those four
    functions in some other refactor work I have planned, and want them
    under unit testing before I proceed.

---
+++ tests/test_util.c
@@ -0,0 +1,127 @@
+/*
+ *
+ * XASTIR, Amateur Station Tracking and Information Reporting
+ * Copyright (C) 2025 The Xastir Group
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Look at the README for more information on the program.
+ */
+
+/*
+ * Test program for object_utils.c functions
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "tests/test_framework.h"
+
+#include "util.h"
+
+int test_convert_lat_l2s_basic(void)
+{
+  long lat;
+  char lat_str[20];
+  // Compute Xastir coordinates for 35d01.631'N
+  // Xastir coordinates are in hundredths of seconds, with 0 being 90d N
+  lat = 90*60*60*100-(35*60+1.631)*60*100;
+  convert_lat_l2s(lat,lat_str, sizeof(lat_str),CONVERT_HP_NOSP);
+  TEST_ASSERT_STR_EQ("3501.631N",lat_str,"xastir y value correctly converted to string");
+  TEST_PASS("convert_lat_l2s: correct");
+}
+int test_convert_lon_l2s_basic(void)
+{
+  long lon;
+  char lon_str[20];
+  // Compute Xastir coordinates for 106d12.385'W
+  // Xastir coordinates are in hundredths of seconds, with 0 being 90d N
+  lon = 180*60*60*100-(106*60+12.385)*60*100;
+  convert_lon_l2s(lon,lon_str, sizeof(lon_str),CONVERT_HP_NOSP);
+  TEST_ASSERT_STR_EQ("10612.385W",lon_str,"xastir x value correctly converted to string");
+  TEST_PASS("convert_lon_l2s: correct");
+}
+int test_convert_lat_s2l_basic(void)
+{
+  long lat;
+  long lat_expect;
+  // Compute Xastir coordinates for 35d01.631'N
+  // Xastir coordinates are in hundredths of seconds, with 0 being 90d N
+  lat_expect = 90*60*60*100-(35*60+1.631)*60*100;
+  lat = convert_lat_s2l("3501.631N");
+  TEST_ASSERT(lat==lat_expect,"xastir y value correctly converted from string");
+  TEST_PASS("convert_lat_s2l: correct");
+}
+int test_convert_lon_s2l_basic(void)
+{
+  long lon;
+  long lon_expect;
+  // Compute Xastir coordinates for 106d12.385'W
+  // Xastir coordinates are in hundredths of seconds, with 0 being 90d N
+  lon_expect = 180*60*60*100-(106*60+12.385)*60*100;
+  lon = convert_lon_s2l("10612.385W");
+  TEST_ASSERT(lon==lon_expect,"xastir x value correctly converted from string");
+  TEST_PASS("convert_lon_s2l: correct");
+}
+
+/* Test runner */
+typedef struct {
+    const char *name;
+    int (*func)(void);
+} test_case_t;
+
+int main(int argc, char *argv[])
+{
+  test_case_t tests[] = {
+    {"convert_lat_l2s_basic",test_convert_lat_l2s_basic},
+    {"convert_lon_l2s_basic",test_convert_lon_l2s_basic},
+    {"convert_lat_s2l_basic",test_convert_lat_s2l_basic},
+    {"convert_lon_s2l_basic",test_convert_lon_s2l_basic},
+    {NULL,NULL}
+  };
+
+
+  if (argc < 2)
+  {
+    fprintf(stderr, "Usage: %s <test name>\n", argv[0]);
+    fprintf(stderr, "Available tests: \n");
+    for (int i = 0; tests[i].name != NULL; i++)
+    {
+      fprintf(stderr, "  %s\n", tests[i].name);
+    }
+    return 1;
+  }
+
+  const char *test_name = argv[1];
+
+  /* Run the requested test */
+  for (int i = 0; tests[i].name != NULL; i++)
+  {
+    if (strcmp(test_name, tests[i].name) == 0)
+    {
+      return tests[i].func();
+    }
+  }
+
+  fprintf(stderr, "Unknown test: %s\n", test_name);
+  return 1;
+}
+
+
